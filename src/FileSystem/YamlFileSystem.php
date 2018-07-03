0a6539bea1f68f406e9a74afa52c904bf9ccf935
Tomas Votruba <tomas.vot@gmail.com>
2018-07-03 21:29:06 +0200

    return YamlFileSystem

--- src/FileSystem/YamlFileSystem.php
+++ src/FileSystem/YamlFileSystem.php
@@ -1,3 +1,17 @@
 <?php declare(strict_types=1);
 
 namespace Fop\FileSystem;
+
+use Symfony\Component\Yaml\Yaml;
+
+final class YamlFileSystem
+{
+    /**
+     * @param mixed[] $data
+     */
+    public function saveArrayToFile(array $data, string $file): void
+    {
+        $yamlDump = Yaml::dump($data, 10, 4);
+        file_put_contents($file, $yamlDump);
+    }
+}
