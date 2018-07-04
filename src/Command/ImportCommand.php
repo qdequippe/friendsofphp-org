af89e33df183fcf24897a5f68bee397e004f0e37
Tomas Votruba <tomas.vot@gmail.com>
2018-07-04 08:41:28 +0200

    add SymfonyStyle and options to ImportCommand

--- src/Command/ImportCommand.php
+++ src/Command/ImportCommand.php
@@ -8,11 +8,23 @@ use Fop\Repository\MeetupRepository;
 use Fop\Repository\UserGroupRepository;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Symplify\PackageBuilder\Console\Command\CommandNaming;
 
 final class ImportCommand extends Command
 {
+    /**
+     * @var string
+     */
+    private const OPTION_GROUPS = 'groups';
+
+    /**
+     * @var string
+     */
+    private const OPTION_MEETUPS = 'meetups';
+
     /**
      * @var GroupsFromPhpUgImporter
      */
@@ -33,28 +45,46 @@ final class ImportCommand extends Command
      */
     private $meetupsFromMeetupComImporter;
 
+    /**
+     * @var SymfonyStyle
+     */
+    private $symfonyStyle;
+
     public function __construct(
         GroupsFromPhpUgImporter $groupsFromPhpUgImporter,
         UserGroupRepository $userGroupRepository,
         MeetupRepository $meetupRepository,
-        MeetupsFromMeetupComImporter $meetupsFromMeetupComImporter
+        MeetupsFromMeetupComImporter $meetupsFromMeetupComImporter,
+        SymfonyStyle $symfonyStyle
     ) {
         parent::__construct();
         $this->groupsFromPhpUgImporter = $groupsFromPhpUgImporter;
         $this->userGroupRepository = $userGroupRepository;
         $this->meetupRepository = $meetupRepository;
         $this->meetupsFromMeetupComImporter = $meetupsFromMeetupComImporter;
+        $this->symfonyStyle = $symfonyStyle;
     }
 
     protected function configure(): void
     {
         $this->setName(CommandNaming::classToName(self::class));
+        $this->addOption(self::OPTION_GROUPS, null, InputOption::VALUE_NONE, 'Imports groups');
+        $this->addOption(self::OPTION_MEETUPS, null, InputOption::VALUE_NONE, 'Imports meetups');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): void
     {
-        $this->importsGroups();
-        $this->importMeetups();
+        if ($input->getOption(self::OPTION_GROUPS)) {
+            $this->symfonyStyle->note('Importing groups from php.ug');
+            $this->importsGroups();
+            $this->symfonyStyle->success('Done');
+        }
+
+        if ($input->getOption(self::OPTION_MEETUPS)) {
+            $this->symfonyStyle->note('Importing meetups from meetup.com');
+            $this->importMeetups();
+            $this->symfonyStyle->success('Done');
+        }
     }
 
     /**
