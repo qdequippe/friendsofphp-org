d2079f7b4e10413aa789889058d897945277efde
Tomas Votruba <tomas.vot@gmail.com>
2018-10-02 11:09:30 +0800

    add more reliable way to determine future distance

---
+++ src/Twig/DatingFilterProvider.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace Fop\Twig;
+
+use Nette\Utils\DateTime;
+use Symplify\Statie\Contract\Templating\FilterProviderInterface;
+
+final class DatingFilterProvider implements FilterProviderInterface
+{
+    /**
+     * @return callable[]
+     */
+    public function provide(): array
+    {
+        return [
+            'diffFromTodayInDays' => function (string $dateTime): int {
+                $dateTime = new DateTime($dateTime);
+                $dateInterval = $dateTime->diff(new DateTime('now'));
+                if (! $dateInterval->invert) {
+                    return - $dateInterval->days;
+                }
+
+                return $dateInterval->days;
+            }
+        ];
+    }
+}
