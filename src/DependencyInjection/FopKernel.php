5b798a0534dfe16b4465651960790eb3bdde57b0
Tomas Votruba <tomas.vot@gmail.com>
2018-10-02 13:19:53 +0800

    make use of Configurable collectors

--- src/DependencyInjection/FopKernel.php
+++ src/DependencyInjection/FopKernel.php
@@ -2,22 +2,16 @@
 
 namespace Fop\DependencyInjection;
 
-use Fop\DependencyInjection\CompilerPass\CollectorCompilerPass;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 use Symfony\Component\HttpKernel\Kernel;
 use Symplify\PackageBuilder\DependencyInjection\CompilerPass\AutoBindParametersCompilerPass;
+use Symplify\PackageBuilder\DependencyInjection\CompilerPass\ConfigurableCollectorCompilerPass;
+use Symplify\PackageBuilder\HttpKernel\SimpleKernelTrait;
 
 final class FopKernel extends Kernel
 {
-    /**
-     * @return BundleInterface[]
-     */
-    public function registerBundles(): array
-    {
-        return [];
-    }
+    use SimpleKernelTrait;
 
     public function registerContainerConfiguration(LoaderInterface $loader): void
     {
@@ -25,19 +19,9 @@ final class FopKernel extends Kernel
         $loader->load(__DIR__ . '/../config/config.yml');
     }
 
-    public function getCacheDir(): string
-    {
-        return sys_get_temp_dir() . '/_fop';
-    }
-
-    public function getLogDir(): string
-    {
-        return sys_get_temp_dir() . '/_fop_logs';
-    }
-
     protected function build(ContainerBuilder $containerBuilder): void
     {
-        $containerBuilder->addCompilerPass(new CollectorCompilerPass());
+        $containerBuilder->addCompilerPass(new ConfigurableCollectorCompilerPass());
         $containerBuilder->addCompilerPass(new AutoBindParametersCompilerPass());
     }
 
