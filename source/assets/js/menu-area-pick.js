80e017009cf980875eb5e173718bc5875afb1722
Tomas Votruba <tomas.vot@gmail.com>
2018-10-18 16:54:48 +0200

    hide tables on no meetups + show help if really no meetup [closes #65]

--- source/assets/js/menu-area-pick.js
+++ source/assets/js/menu-area-pick.js
@@ -10,7 +10,34 @@ $(function() {
                 $(this).hide();
             }
         });
-    }
+
+        // see https://stackoverflow.com/a/20064911/1348344
+        var nextWeekVisibleMeetups = $("table#table-week tr:visible").length - 1;
+        var nextMonthVisibleMeetups = $("table#table-month tr:visible").length - 1;
+
+        console.log(nextWeekVisibleMeetups);
+        console.log(nextMonthVisibleMeetups);
+
+        if (nextWeekVisibleMeetups === 0) {
+            // hide week tab
+            $("#table-block-week").hide();
+        } else {
+            $("#table-block-week").show();
+        }
+
+        if (nextMonthVisibleMeetups === 0) {
+            // hide month tab
+            $("#table-block-month").hide();
+        } else {
+            $("#table-block-month").show();
+        }
+
+        if (nextWeekVisibleMeetups === 0 && nextMonthVisibleMeetups === 0) {
+            $("#block-zoomout").show();
+        } else {
+            $("#block-zoomout").hide();
+        }
+    };
 
     // show relevant meetups when map moves
     map.on('moveend', function() {
