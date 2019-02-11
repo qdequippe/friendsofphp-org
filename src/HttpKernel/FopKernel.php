269dba900e827f58add6f60fa71f15f5e6e6c86d
Tomas Votruba <tomas.vot@gmail.com>
2019-02-11 22:35:09 +0100

    drop ContainerFactory and use FopKernel directly

--- src/HttpKernel/FopKernel.php
+++ src/HttpKernel/FopKernel.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Fop\DependencyInjection;
+namespace Fop\HttpKernel;
 
 use Symfony\Component\Config\Loader\DelegatingLoader;
 use Symfony\Component\Config\Loader\GlobFileLoader;
@@ -8,38 +8,21 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\Config\Loader\LoaderResolver;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 use Symfony\Component\HttpKernel\Config\FileLocator;
 use Symfony\Component\HttpKernel\Kernel;
 use Symplify\PackageBuilder\DependencyInjection\CompilerPass\AutoBindParametersCompilerPass;
 use Symplify\PackageBuilder\DependencyInjection\CompilerPass\AutoReturnFactoryCompilerPass;
 use Symplify\PackageBuilder\DependencyInjection\CompilerPass\AutowireArrayParameterCompilerPass;
 use Symplify\PackageBuilder\DependencyInjection\CompilerPass\ConfigurableCollectorCompilerPass;
-use Symplify\PackageBuilder\HttpKernel\SimpleKernelTrait;
 use Symplify\PackageBuilder\Yaml\FileLoader\ParameterMergingYamlFileLoader;
 
 final class FopKernel extends Kernel
 {
-    use SimpleKernelTrait;
-
-    /**
-     * @var string|null
-     */
-    private $config;
-
     public function registerContainerConfiguration(LoaderInterface $loader): void
     {
         $loader->load(__DIR__ . '/../../packages/*/config/config.yaml', 'glob');
         $loader->load(__DIR__ . '/../../config/config.yaml');
-
-        if ($this->config) {
-            $loader->load($this->config);
-        }
-    }
-
-    public function bootWithConfig(string $config): void
-    {
-        $this->config = $config;
-        $this->boot();
     }
 
     protected function build(ContainerBuilder $containerBuilder): void
@@ -62,5 +45,23 @@ final class FopKernel extends Kernel
         ]);
 
         return new DelegatingLoader($loaderResolver);
+    }
+
+    public function getCacheDir(): string
+    {
+        return sys_get_temp_dir() . '/fop_cache';
+    }
+
+    public function getLogDir(): string
+    {
+        return sys_get_temp_dir() . '/fop_log';
+    }
+
+    /**
+     * @return BundleInterface[]
+     */
+    public function registerBundles(): iterable
+    {
+        return [];
     }
 }
