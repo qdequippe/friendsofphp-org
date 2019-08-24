9d12cb2cdfc6941eec93ae20a60d11fbd4c7146b
Tomas Votruba <tomas.vot@gmail.com>
2019-08-24 15:27:23 +0200

    remove generate-first, improve headline, make json less angry

--- gulpfile.js
+++ gulpfile.js
@@ -7,21 +7,13 @@ var exec = require('child_process').exec;
 
 gulp.task('default', function () {
     // Generate current version
-    exec('bin/generate-first', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.error(stderr);
-        if (err !== '') {
-            throw new Error();
-        }
-    });
-
     exec('vendor/bin/statie generate source', function (err, stdout, stderr) {
         console.log(stdout);
         console.error(stderr);
     });
 
     // Run local server, open localhost:8000 in your browser
-    exec('php -S localhost:8000 -t output');
+    exec('php -S localhost:8888 -t output');
 
     // For the second arg see: https://github.com/floatdrop/gulp-watch/issues/242#issuecomment-230209702
     return watch(['source/**/*', '!**/*___jb_tmp___'], { ignoreInitial: false })
