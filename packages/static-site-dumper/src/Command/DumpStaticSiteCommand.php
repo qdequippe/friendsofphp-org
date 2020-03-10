666d88875f735116ffa1a5fd7d10aee4a65504da
TomasVotruba <tomas.vot@gmail.com>
2020-03-10 16:44:29 +0100

    dumper

--- packages/static-site-dumper/src/Command/DumpStaticSiteCommand.php
+++ packages/static-site-dumper/src/Command/DumpStaticSiteCommand.php
@@ -4,14 +4,40 @@ declare(strict_types=1);
 
 namespace Fop\StaticSiteDumper\Command;
 
+use Nette\Utils\FileSystem;
+use Psr\Container\ContainerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Routing\RouterInterface;
 use Symplify\PackageBuilder\Console\Command\CommandNaming;
 use Symplify\PackageBuilder\Console\ShellCode;
 
 final class DumpStaticSiteCommand extends Command
 {
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+    /**
+     * @var SymfonyStyle
+     */
+    private $symfonyStyle;
+
+    public function __construct(RouterInterface $router, ContainerInterface $container, SymfonyStyle $symfonyStyle)
+    {
+        $this->router = $router;
+        $this->container = $container;
+        $this->symfonyStyle = $symfonyStyle;
+
+        parent::__construct();
+    }
+
     protected function configure(): void
     {
         $this->setName(CommandNaming::classToName(self::class));
@@ -20,6 +46,19 @@ final class DumpStaticSiteCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $output->writeln('yes');
+
+        foreach ($this->router->getRouteCollection() as $route) {
+            dump($route->getPath());
+
+            $controllerClass = $route->getDefault('_controller');
+            $controller = $this->container->get($controllerClass);
+            dump((string) $controller());
+
+            FileSystem::write( getcwd() . '/output/' . );
+
+
+            die;
+        }
 
         return ShellCode::SUCCESS;
     }
