3cfa4e62f1f2fa9e6595bfff855f5c708b80ce7d
TomasVotruba <tomas.vot@gmail.com>
2020-03-10 16:55:50 +0100

    add static site dumper

--- packages/static-site-dumper/src/Command/DumpStaticSiteCommand.php
+++ packages/static-site-dumper/src/Command/DumpStaticSiteCommand.php
@@ -4,38 +4,58 @@ declare(strict_types=1);
 
 namespace Fop\StaticSiteDumper\Command;
 
+use Fop\StaticSiteDumper\HttpFoundation\ControllerContentResolver;
 use Nette\Utils\FileSystem;
-use Psr\Container\ContainerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouterInterface;
 use Symplify\PackageBuilder\Console\Command\CommandNaming;
 use Symplify\PackageBuilder\Console\ShellCode;
+use Symplify\SmartFileSystem\Finder\FinderSanitizer;
 
 final class DumpStaticSiteCommand extends Command
 {
     /**
+     * @var string
+     */
+    public const PUBLIC_DIRECTORY = __DIR__ . '/../../../../public';
+
+    /**
      * @var RouterInterface
      */
     private $router;
-    /**
-     * @var ContainerInterface
-     */
-    private $container;
+
     /**
      * @var SymfonyStyle
      */
     private $symfonyStyle;
 
-    public function __construct(RouterInterface $router, ContainerInterface $container, SymfonyStyle $symfonyStyle)
-    {
-        $this->router = $router;
-        $this->container = $container;
-        $this->symfonyStyle = $symfonyStyle;
+    /**
+     * @var ControllerContentResolver
+     */
+    private $controllerContentResolver;
 
+    /**
+     * @var FinderSanitizer
+     */
+    private $finderSanitizer;
+
+    public function __construct(
+        RouterInterface $router,
+        SymfonyStyle $symfonyStyle,
+        ControllerContentResolver $controllerContentResolver,
+        FinderSanitizer $finderSanitizer
+    ) {
         parent::__construct();
+
+        $this->router = $router;
+        $this->symfonyStyle = $symfonyStyle;
+        $this->controllerContentResolver = $controllerContentResolver;
+        $this->finderSanitizer = $finderSanitizer;
     }
 
     protected function configure(): void
@@ -45,24 +65,60 @@ final class DumpStaticSiteCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->symfonyStyle->section(sprintf(
-            'Dumping static website for %d routes', count($this->router->getRouteCollection()))
-        );
+        $this->symfonyStyle->section('Dumping static website');
 
-        foreach ($this->router->getRouteCollection() as $route) {
-            $controllerClass = $route->getDefault('_controller');
+        $this->dumpControllerContents();
+        $this->copyAssets();
 
-            $this->symfonyStyle->note(sprintf('Dumping static content for "%s" class to "%s" path', $controllerClass, $route->getPath()));
-
-            $controller = $this->container->get($controllerClass);
-            dump((string) $controller());
-
-            FileSystem::write( getcwd() . '/output/' . );
-
-
-            die;
-        }
+        $this->symfonyStyle->success('Done!');
 
         return ShellCode::SUCCESS;
+    }
+
+    private function dumpControllerContents(): void
+    {
+        foreach ($this->router->getRouteCollection() as $route) {
+            $fileContent = $this->controllerContentResolver->resolveFromRoute($route);
+            if ($fileContent === null) {
+                continue;
+            }
+
+            $filePath = $this->resolveFilePath($route);
+
+            $this->symfonyStyle->note(sprintf(
+                'Dumping static content for"%s" route to "%s" path',
+                $route->getPath(),
+                $filePath
+            ));
+
+            FileSystem::write($filePath, $fileContent);
+        }
+    }
+
+    private function copyAssets(): void
+    {
+        $finder = new Finder();
+        $finder->files()
+            ->in(self::PUBLIC_DIRECTORY)
+            ->notName('*.php');
+
+        $assetFileInfos = $this->finderSanitizer->sanitize($finder);
+        foreach ($assetFileInfos as $assetFileInfo) {
+            $relativePathFromRoot = $assetFileInfo->getRelativeFilePathFromDirectory(self::PUBLIC_DIRECTORY);
+            FileSystem::copy($assetFileInfo->getRealPath(), getcwd() . '/output/' . $relativePathFromRoot);
+        }
+    }
+
+    private function resolveFilePath(Route $route): string
+    {
+        $routePath = $route->getPath();
+
+        if ($routePath === '/') {
+            $routePath = '/index.html';
+        } else {
+            $routePath .= '/index.html';
+        }
+
+        return getcwd() . '/output' . $routePath;
     }
 }
