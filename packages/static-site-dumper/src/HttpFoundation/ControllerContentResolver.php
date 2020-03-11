9276be0346f9c0ca6c85d1bbc12933b2871d718f
TomasVotruba <tomas.vot@gmail.com>
2020-03-11 18:56:23 +0100

    make dumper ready for non-invokable controllers

--- packages/static-site-dumper/src/HttpFoundation/ControllerContentResolver.php
+++ packages/static-site-dumper/src/HttpFoundation/ControllerContentResolver.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Fop\StaticSiteDumper\HttpFoundation;
 
+use Nette\Utils\Strings;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Route;
@@ -22,14 +23,23 @@ final class ControllerContentResolver
 
     public function resolveFromRoute(Route $route): ?string
     {
-        $controllerClass = $route->getDefault('_controller');
+        $controller = $route->getDefault('_controller');
+
+        if (Strings::contains($controller, '::')) {
+            [$controllerClass, $method] = Strings::split($controller, '#::#');
+        } else {
+            $controllerClass = $controller;
+            $method = '__invoke';
+        }
+
         if (! class_exists($controllerClass)) {
             return null;
         }
 
         $controller = $this->container->get($controllerClass);
+
         /** @var Response $response */
-        $response = $controller();
+        $response = call_user_func([$controller, $method]);
 
         return (string) $response->getContent();
     }
