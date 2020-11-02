9598de4528cc66b84ce5c271d00dcdcb68526aad
Tomas Votruba <tomas.vot@gmail.com>
2020-11-03 00:46:07 +0100

    cleanup

--- config/bundles.php
+++ config/bundles.php
@@ -1,22 +1,8 @@
-<?php declare(strict_types=1);
+<?php
 
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => [
-        'all' => true,
-    ],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => [
-        'dev' => true,
-        'test' => true,
-    ],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => [
-        'all' => true,
-    ],
-    Symplify\ConsoleColorDiff\ConsoleColorDiffBundle::class => [
-        'dev' => true,
-        'test' => true,
-    ],
-    Symplify\ComposerJsonManipulator\ComposerJsonManipulatorBundle::class => [
-        'dev' => true,
-        'test' => true,
-    ],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symplify\ConsoleColorDiff\ConsoleColorDiffBundle::class => ['dev' => true, 'test' => true],
 ];
