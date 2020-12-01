e747dc3e2feddec1b883291bc8ca3db67e3aaa72
TomasVotruba <tomas.vot@gmail.com>
2020-12-02 00:17:04 +0100

    update compiler pass command

--- config/bundles.php
+++ config/bundles.php
@@ -2,8 +2,11 @@
 
 declare(strict_types=1);
 
+use Symplify\SymfonyStaticDumper\SymfonyStaticDumperBundle;
+
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    SymfonyStaticDumperBundle::class => ['all' => true],
 ];
--- config/config.php
+++ config/config.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 use GuzzleHttp\Client;
 use GuzzleHttp\ClientInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\PackageBuilder\Console\Style\SymfonyStyleFactory;
 use Symplify\PackageBuilder\Http\BetterGuzzleClient;
 use Symplify\PackageBuilder\Parameter\ParameterProvider;
 use Symplify\PackageBuilder\Reflection\PrivatesAccessor;
-use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/parameters.php');
@@ -34,10 +31,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(ParameterProvider::class);
 
     $services->set(PrivatesAccessor::class);
-
-    $services->set(SymfonyStyle::class)
-        ->factory([service(SymfonyStyleFactory::class), 'create']);
-    $services->set(SymfonyStyleFactory::class);
 
     $services->set(Client::class);
     $services->set(BetterGuzzleClient::class);
