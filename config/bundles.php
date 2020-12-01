7cced5be13035541fac5f8872f5e4f01062aa10c
TomasVotruba <tomas.vot@gmail.com>
2020-12-01 22:57:58 +0100

    bump rector

--- config/bundles.php
+++ config/bundles.php
@@ -1,9 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symplify\ConsoleColorDiff\ConsoleColorDiffBundle::class => ['dev' => true, 'test' => true],
-    Symplify\ComposerJsonManipulator\ComposerJsonManipulatorBundle::class => ['all' => true],
 ];
