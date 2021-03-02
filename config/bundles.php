67d8b2c1551412d257519a17734fe66d1b1e3f9d
TomasVotruba <tomas.vot@gmail.com>
2021-03-02 10:53:39 +0100

    same seupt

--- config/bundles.php
+++ config/bundles.php
@@ -1,13 +1,11 @@
 <?php
 
-use Symplify\SimplePhpDocParser\Bundle\SimplePhpDocParserBundle;
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symplify\SymfonyStaticDumper\SymfonyStaticDumperBundle::class => ['all' => true],
     Symplify\EasyHydrator\EasyHydratorBundle::class => ['all' => true],
-    SimplePhpDocParserBundle::class => ['all' => true],
-    \Symplify\PhpConfigPrinter\Bundle\PhpConfigPrinterBundle::class => ['all' => true],
+    Symplify\SimplePhpDocParser\Bundle\SimplePhpDocParserBundle::class => ['all' => true],
+    Symplify\PhpConfigPrinter\Bundle\PhpConfigPrinterBundle::class => ['all' => true],
 ];
