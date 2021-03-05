6537ce692099a85505802498448a573dbb38d29c
TomasVotruba <tomas.vot@gmail.com>
2021-03-06 00:57:18 +0100

    remove composition

--- src/Controller/ApiGroupsController.php
+++ src/Controller/ApiGroupsController.php
@@ -4,23 +4,25 @@ declare(strict_types=1);
 
 namespace Fop\Core\Controller;
 
+use Fop\Core\Templating\ResponseRenderer;
 use Fop\Core\ValueObject\Option;
 use Fop\Core\ValueObject\Routing\RouteName;
 use Nette\Utils\DateTime;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symplify\PackageBuilder\Parameter\ParameterProvider;
 
-final class ApiGroupsController extends AbstractController
+final class ApiGroupsController
 {
     /**
      * @var mixed[]
      */
     private array $groups = [];
 
-    public function __construct(ParameterProvider $parameterProvider)
-    {
+    public function __construct(
+        ParameterProvider $parameterProvider,
+        private ResponseRenderer $responseRenderer
+    ) {
         $this->groups = $parameterProvider->provideArrayParameter(Option::GROUPS);
     }
 
@@ -32,7 +34,8 @@ final class ApiGroupsController extends AbstractController
     public function __invoke(): Response
     {
         $generatedAt = DateTime::from('now')->format('Y-m-d H:i:s');
-        return $this->json([
+
+        return $this->responseRenderer->json([
             'generated_at' => $generatedAt,
             'group_count' => count($this->groups),
             'groups' => $this->groups,
--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -4,23 +4,24 @@ declare(strict_types=1);
 
 namespace Fop\Core\Controller;
 
+use Fop\Core\Templating\ResponseRenderer;
 use Fop\Core\ValueObject\Routing\RouteName;
 use Fop\Meetup\Repository\MeetupRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-final class HomepageController extends AbstractController
+final class HomepageController
 {
     public function __construct(
-        private MeetupRepository $meetupRepository
+        private MeetupRepository $meetupRepository,
+        private ResponseRenderer $responseRenderer
     ) {
     }
 
     #[Route('/', name: RouteName::HOMEPAGE)]
     public function __invoke(): Response
     {
-        return $this->render('index.twig', [
+        return $this->responseRenderer->render('index.twig', [
             'meetups' => $this->meetupRepository->fetchAll(),
         ]);
     }
