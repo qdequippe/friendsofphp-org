ba852920340485a352562f1aa42f12a0e05c1612
Tomas Votruba <tomas.vot@gmail.com>
2022-02-07 10:35:45 +0100

    cleanup amnesia

--- config/packages/framework.php
+++ config/packages/framework.php
@@ -3,10 +3,9 @@
 declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\Amnesia\ValueObject\Symfony\Extension\FrameworkExtension;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->extension(FrameworkExtension::NAME, [
-        FrameworkExtension::SECRET => '%env(APP_SECRET)%',
+    $containerConfigurator->extension('framework', [
+        'secret' => '%env(APP_SECRET)%',
     ]);
 };
--- config/packages/twig.php
+++ config/packages/twig.php
@@ -3,13 +3,12 @@
 declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\Amnesia\ValueObject\Symfony\Extension\TwigExtension;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->extension(TwigExtension::NAME, [
-        TwigExtension::DEBUG => '%kernel.debug%',
-        TwigExtension::STRICT_VARIABLES => '%kernel.debug%',
-        TwigExtension::GLOBALS => [
+    $containerConfigurator->extension('twig', [
+        'debug' => true,
+        'strict_variables' => true,
+        'globals' => [
             'max_forecast_days' => '%max_forecast_days%',
             'map_id' => 'mapid',
         ],
