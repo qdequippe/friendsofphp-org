bf0bd7f65c682fb2414c05bcee8c3a3a7b2fe842
Tomas Votruba <tomas.vot@gmail.com>
2022-02-09 15:07:12 +0100

    simplify configs

--- config/config.php
+++ config/config.php
@@ -7,7 +7,6 @@ use Jajo\JSONDB;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(__DIR__ . '/packages/*');
     $containerConfigurator->import(__DIR__ . '/../packages/*/config/*');
 
     $services = $containerConfigurator->services();
@@ -24,4 +23,16 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $services->set(JSONDB::class)
         ->arg('$dir', __DIR__ . '/../json-database');
+
+    $containerConfigurator->extension('framework', [
+        'secret' => '%env(APP_SECRET)%',
+    ]);
+
+    $containerConfigurator->extension('twig', [
+        'debug' => true,
+        'strict_variables' => true,
+        'globals' => [
+            'map_id' => 'mapid',
+        ],
+    ]);
 };
