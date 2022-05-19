c859d8e478e017d711a5400a56d97317baffaadd
Tomas Votruba <tomas.vot@gmail.com>
2022-05-19 15:20:55 +0200

    fix ecs

--- ecs.php
+++ ecs.php
@@ -2,19 +2,14 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\CodingStandard\Fixer\LineLength\LineLengthFixer;
-use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $services = $containerConfigurator->services();
-    $services->set(LineLengthFixer::class);
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->rule(LineLengthFixer::class);
 
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PARALLEL, true);
-
-    $parameters->set(Option::PATHS, [
+    $ecsConfig->paths([
         __DIR__ . '/bin',
         __DIR__ . '/config',
         __DIR__ . '/src',
@@ -24,10 +19,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         __DIR__ . '/rector.php',
     ]);
 
-    $containerConfigurator->import(SetList::CLEAN_CODE);
-    $containerConfigurator->import(SetList::COMMON);
-    $containerConfigurator->import(SetList::SYMPLIFY);
-    $containerConfigurator->import(SetList::PSR_12);
+    $ecsConfig->sets([SetList::CLEAN_CODE, SetList::COMMON, SetList::SYMPLIFY, SetList::PSR_12]);
 
-    $parameters->set(Option::SKIP, [__DIR__ . '/config/bundles.php']);
+    $ecsConfig->skip([__DIR__ . '/config/bundles.php']);
 };
