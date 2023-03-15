9fc634bff4aafefc92ec9580ebebacd4e44a52c6
Quentin Dequippe <quentin@dequippe.tech>
2023-03-15 21:24:51 +0400

    Fix encoding bug and cleaning (#223)
    * Fix encoding bug and cleaning
    
    * run rector
    
    * remove Option

--- src/Exception/ShouldNotHappenException.php
+++ src/Exception/ShouldNotHappenException.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Fop\Exception;
 
 use Exception;
-
 final class ShouldNotHappenException extends Exception
 {
 }
--- src/Meetup/Contract/ArrayableInterface.php
+++ src/Meetup/Contract/ArrayableInterface.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Fop\Meetup\Contract;
 
 use JsonSerializable;
-
 interface ArrayableInterface extends JsonSerializable
 {
     /**
