064a021532ace9a3f9ac8fa0855db4a53e791d6e
Quentin Dequippe <quentin@dequippe.tech>
2023-06-05 19:50:10 +0400

    replace symplify/symfony-static-dumper by stenope

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Stenope\Bundle\StenopeBundle::class => ['all' => true],
 ];
